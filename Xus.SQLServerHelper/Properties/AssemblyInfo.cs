b8b8a36bb56e57ecb89cccf5e544e9401738fc47
Xusheng <jsksxs360@163.com>
2016-11-15 15:09:22 +0800

    update version

--- Xus.SQLServerHelper/Properties/AssemblyInfo.cs
+++ Xus.SQLServerHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Xus.SQLServerHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SQL Server Helper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("XiaoSheng")]
 [assembly: AssemblyProduct("Xus.SQLServerHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  XiaoSheng 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
